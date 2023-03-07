057c515614dd019005a67a80a98192359691596b
4hebul3 <123885270+4hebul3@users.noreply.github.com>
2023-03-08 00:35:48 +0300

    Read below
    Files:Controller.java, Form.java, Main.java and Tetris.java - need to move it to a folder

---
+++ module-info.java
@@ -0,0 +1,12 @@
+module tetrix {
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.media;
+    requires javafx.swing;
+    requires javafx.web;
+    requires javafx.swt;
+
+    opens apllication;
+}
